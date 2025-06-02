619306ffaa88fb97e72b271908e1f4d4c6211964
kish <kish@gmail.com>
2025-06-02 16:10:22 +0545

    Add JobRepository interface

---
+++ src/main/java/com/kish/jobtracker/dto/JobDTO.java
@@ -0,0 +1,50 @@
+package com.kish.jobtracker.dto;
+
+
+public class JobDTO {
+    private String title;
+    private String company;
+    private String status;
+    private String dateApplied;
+    private String notes;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDateApplied() {
+        return dateApplied;
+    }
+
+    public void setDateApplied(String dateApplied) {
+        this.dateApplied = dateApplied;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
